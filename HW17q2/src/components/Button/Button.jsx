738154bdac0bc158fb7a46baea7f49be08d23fe9
Arian Farzanehnejad <afn2681@gmail.com>
2023-04-27 19:32:06 +0330

    add some changes

--- HW17q2/src/components/Button/Button.jsx
+++ HW17q2/src/components/Button/Button.jsx
@@ -12,21 +12,30 @@ export default function Button() {
   }
   return (
     <>
-      <div className='flex justify-center'>
+      <div className='flex justify-center pt-12'>
         <div className="">
           <button 
-            className='text-4xl p-3 mb-10 bg-blue-400 text-white rounded-full'
+            className='text-4xl p-5 mb-4 bg-blue-400 text-white rounded-full'
             onClick={handleClick}
             >Click ME!
           </button>
           <ul>
-            {itemcolor.map((item,index)=><li key={index} className={`rounded-full p-4 text-center text-white text-4xl`} style={{backgroundColor:`${item}`}}>{item}</li>)}          
+            {itemcolor.map((item,index)=><li key={index} className={`rounded-full p-5 mb-4 text-center text-white text-4xl`} style={{backgroundColor:`${item}`}}>{item}</li>)}          
           </ul>
         </div>
       </div>
     </>
   )
 }
+
+
+
+
+
+
+
+
+
 
 
 // import { useState } from "react";
--- HW17q3/src/components/Buttons/Buttons.jsx
+++ HW17q3/src/components/Buttons/Buttons.jsx
@@ -1,70 +1,179 @@
 import { useState } from "react"
 export default function Buttons() {
-    let red=149
-    let blue=149
-    let green=149
-    let [background , setBackground]=useState(`rgb(${red},${green},${blue})`)
-    // let rgb=`rgb(${red},${green},${blue})`
-    function addRED()
-    {
-        red++;
-        calculateSum()
-    }
-    function reduceRED()
-    {
-        red--;
-        calculateSum()
-    }
-    function addBlue()
-    {
-        blue++;
-        calculateSum()
-    }
-    function reduceBlue()
-    {
-        blue--;
-        calculateSum()
-    }
-    function addGreen()
-    {
-        green++;
-        calculateSum()
-    }
-    function reduceGreen()
-    {
-        green--;
-        calculateSum()
-    }
-    function calculateSum()
-    {
-        setBackground(`rgb(${red},${green},${blue})`)
-    }
+    const [blueColor , setBlueColor]=useState(0)
+    const [redColor , setRedColor]=useState(0)
+    const [greenColor , setGreenColor]=useState(0)
   return (
       <>
         <div className="flex w-full">
             <div className="w-[50%] text-6xl text-center">
-                <div className="red-box h-[33vh]">
-                    <div className="red-title">Red</div>
-                    <button onClick={addRED}>+</button>
-                    <button onClick={reduceRED}>-</button>
-                </div>
-                <div className="blue-box h-[33vh]">
-                <div className="Blue-title">Blue</div>
-                    <button onClick={addBlue}>+</button>
-                    <button onClick={reduceBlue}>-</button>
-                </div>
-                <div className="green-box h-[33vh]">
-                <div className="Green-title">Green</div>
-                    <button onClick={addGreen}>+</button>
-                    <button onClick={reduceGreen}>-</button>
-                </div>
+
+
+                <div className="flex flex-col justify-center"><div className="red-box h-[33.33vh] bg-red-500 text-white">
+                    <div className="mb-8 mt-4">Red</div>
+                    <button className="border-white border w-80 rounded-t-full" onClick={()=>{if(redColor<256)setRedColor(redColor+1)}}>+</button><br />
+                    <button className="border-white border w-80 rounded-b-full" onClick={()=>{if(redColor>0)setRedColor(redColor-1)}}>-</button>
+                </div></div>
+
+
+                <div className="flex flex-col justify-center"><div className="green-box h-[33.33vh] bg-green-500 text-white">
+                <div className="mb-8 mt-4">Green</div>
+                    <button className="border-white border w-80 rounded-t-full" onClick={()=>{if(greenColor<256)setGreenColor(greenColor+1)}}>+</button><br />
+                    <button className="border-white border w-80 rounded-b-full" onClick={()=>{if(greenColor>0)setGreenColor(greenColor-1)}}>-</button>
+                </div></div>
+
+
+                <div className="flex flex-col justify-center"><div className="blue-box h-[33.33vh] bg-blue-500 text-white">
+                <div className="mb-8 mt-4">Blue</div>
+                    <button className="border-white border w-80 rounded-t-full" onClick={()=>{if(blueColor<256)setBlueColor(blueColor+1)}}>+</button><br />
+                    <button className="border-white border w-80 rounded-b-full" onClick={()=>{if(blueColor>0)setBlueColor(blueColor-1)}}>-</button>
+                </div></div>
+
+
             </div>
+
             <div className={`w-[50%] flex flex-col justify-center`}>
-                <div className="flex justify-center">
-                    <div className="w-[80%] h-44 text-center border-black border text-white text-5xl" style={{backgroundColor:`${background}`}}>{background}</div>
+                <div className="flex flex-col justify-center">
+                    <div className="flex justify-center">
+                        <div className="w-[80%] rounded-full pt-14 h-44 text-center border-black border text-white text-5xl font-bold" style={{backgroundColor:`rgb(${redColor},${greenColor},${blueColor})`}}>{`RGB(${redColor},${greenColor},${blueColor})`}</div>
+                    </div>
                 </div>
             </div>
         </div>
     </>
   )
 }
+
+
+
+
+
+
+
+
+
+// function addRED()
+    // {
+    //     if(redColor<256)
+    //     {
+    //         setRedColor(redColor+1)
+    //     }
+    // }
+    // function reduceRED()
+    // {
+    //     if(redColor>0)
+    //     {
+    //         setRedColor(redColor-1)
+    //     }
+        
+    // }
+
+    // function addGreen()
+    // {
+    //     if(greenColor<256)
+    //     {
+    //         setGreenColor(greenColor+1)
+    //     }
+    // }
+    // function reduceGreen()
+    // {
+    //     if(greenColor>0)
+    //     {
+    //         setGreenColor(greenColor-1)
+    //     }
+    // }
+    
+    // function addBlue()
+    // {
+    //     if(blueColor<256)
+    //     {
+    //         setBlueColor(blueColor+1)
+    //     }
+    // }
+    // function reduceBlue()
+    // {
+    //     if(blueColor>0)
+    //     {
+    //         setBlueColor(blueColor-1)
+    //     }
+    // }
+
+
+
+
+
+
+
+
+
+///////////////////////////////////////////////
+// import { useState } from "react"
+// export default function Buttons() {
+//     let red=149
+//     let blue=149
+//     let green=149
+//     let [background , setBackground]=useState(`rgb(${red},${green},${blue})`)
+//     // let rgb=`rgb(${red},${green},${blue})`
+//     function addRED()
+//     {
+//         red++;
+//         calculateSum()
+//     }
+//     function reduceRED()
+//     {
+//         red--;
+//         calculateSum()
+//     }
+//     function addBlue()
+//     {
+//         blue++;
+//         calculateSum()
+//     }
+//     function reduceBlue()
+//     {
+//         blue--;
+//         calculateSum()
+//     }
+//     function addGreen()
+//     {
+//         green++;
+//         calculateSum()
+//     }
+//     function reduceGreen()
+//     {
+//         green--;
+//         calculateSum()
+//     }
+//     function calculateSum()
+//     {
+//         setBackground(`rgb(${red},${green},${blue})`)
+//     }
+//   return (
+//       <>
+//         <div className="flex w-full">
+//             <div className="w-[50%] text-6xl text-center">
+//                 <div className="red-box h-[33vh]">
+//                     <div className="red-title">Red</div>
+//                     <button onClick={addRED}>+</button>
+//                     <button onClick={reduceRED}>-</button>
+//                 </div>
+//                 <div className="blue-box h-[33vh]">
+//                 <div className="Blue-title">Blue</div>
+//                     <button onClick={addBlue}>+</button>
+//                     <button onClick={reduceBlue}>-</button>
+//                 </div>
+//                 <div className="green-box h-[33vh]">
+//                 <div className="Green-title">Green</div>
+//                     <button onClick={addGreen}>+</button>
+//                     <button onClick={reduceGreen}>-</button>
+//                 </div>
+//             </div>
+//             <div className={`w-[50%] flex flex-col justify-center`}>
+//                 <div className="flex justify-center">
+//                     <div className="w-[80%] h-44 text-center border-black border text-white text-5xl" style={{backgroundColor:`${background}`}}>{background}</div>
+//                 </div>
+//             </div>
+//         </div>
+//     </>
+//   )
+// }
